c64714423b28dc4a2a3fff9b814a61208653b622
Anthony <Knoll>
2017-07-24 15:17:38 -0600

    learning testing for angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    ConvertToEuroPipe
   ],
   imports: [
     BrowserModule
